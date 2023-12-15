2aa713a838df42dc696eb90908da1b153b0bd96f
DimitrisDalis1 <121684210+DimitrisDalis1@users.noreply.github.com>
2023-12-15 16:18:31 +0200

    Add files via upload

---
+++ HY359_PROJECT_POG/src/main/java/servlets/Login.java
@@ -0,0 +1,96 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import database.tables.EditPetKeepersTable;
+import database.tables.EditPetOwnersTable;
+import mainClasses.PetKeeper;
+import mainClasses.PetOwner;
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		  HttpSession session=request.getSession();
+		  EditPetOwnersTable tableO = new EditPetOwnersTable();
+		  EditPetKeepersTable tableK = new EditPetKeepersTable();
+		  PetKeeper p = null;
+		  PetOwner o = null;
+	        if(session.getAttribute("loggedIn")!=null){
+	           response.setStatus(200);
+	           String username = session.getAttribute("loggedIn").toString();
+	           
+			try {
+				p = tableK.databaseToPetKeepersUsernameOnly(username);
+				if(p==null) {
+					o = tableO.databaseToPetOwnersWithUsernameOnly(username);
+				}
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+				if(p == null) {
+					 response.getWriter().write(o.getUsername());
+				}else {
+					 response.getWriter().write(p.getUsername());
+				}
+	          
+	        }
+	        else{
+	            response.setStatus(403);
+	        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        EditPetOwnersTable tableO = new EditPetOwnersTable();
+		EditPetKeepersTable tableK = new EditPetKeepersTable();
+        HttpSession session=request.getSession();
+        
+        try {
+			if(tableO.databaseToPetOwners(username, password)==null && tableK.databaseToPetKeepers(username, password)==null) {
+				response.setStatus(403);
+				response.getWriter().write("There is no pet keeper or pet owner with that username and password");
+			}else {
+				session.setAttribute("loggedIn",username);
+				response.setStatus(200);
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
