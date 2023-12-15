304606c3b8b29b09fbfcbc360eb9bd006dcf73b9
DimitrisDalis1 <121684210+DimitrisDalis1@users.noreply.github.com>
2023-12-15 15:42:26 +0200

    Add files via upload

---
+++ HY359_PROJECT_POG/src/main/webapp/js/ajax.js
@@ -0,0 +1,24 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+function showPetKeeperRegistrationForm(){
+    $("#ajaxContent").load("RegisterPetOwner.html");
+}
+
+function showLoginForm(){
+    $("#ajaxContent").load("Login.html");
+}
+
+
+
+function showHomePage(){
+    $("#ajaxContent").load("index.html");
+}
+
+function showClientPage(){
+    $("#ajaxContent").load("client.html");
+}
+
+
